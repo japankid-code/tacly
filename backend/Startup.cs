b9e5d44dfb15b6eb1fbf511f6ff2a0a740af84ff
japankid-code <japankid.jake@gmail.com>
2021-08-23 18:55:04 -0500

    add dbContext plus auth and migrations

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -1,6 +1,9 @@
+using backend.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,9 @@ namespace backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContextPool<ApplicationDbContext>
+                (options => options.UseSqlServer(Configuration.GetConnectionString("ApplicationConnectionString")));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +51,7 @@ namespace backend
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

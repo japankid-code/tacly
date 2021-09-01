a48d47484b03a5e41513a358cf5324e343fa3638
japankid-code <japankid.jake@gmail.com>
2021-08-31 19:51:01 -0500

    create app from `dotnet new webapp --auth Individual -o backendv2`

--- backendv2/Program.cs
+++ backendv2/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace backend
+namespace backendv2
 {
     public class Program
     {

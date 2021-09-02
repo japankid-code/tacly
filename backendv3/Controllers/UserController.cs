c8f183921920d64659b6977fec7438554e8fb7fe
japankid-code <japankid.jake@gmail.com>
2021-09-01 22:47:27 -0500

    :beers:rework to web API backendv3 thanks @datbear

---
+++ backendv3/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using backendv3.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backendv3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            // return await _dbContext.User.ToListAsync();
+            return Ok(new { data = "hello world" });
+        }
+    }
+}
